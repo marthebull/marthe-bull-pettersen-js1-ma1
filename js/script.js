d4156822f593fc58fe01df58b1aa002fea553e0a
marthebull <marthebullpettersen@gmail.com>
2022-01-30 13:12:19 +0100

    bnm,

---
+++ js/script.js
@@ -0,0 +1,77 @@
+
+// Question 1
+console.log ("Question: 1");
+
+const cat = { complain : console.log("Meow!")}
+
+
+
+// Question 2
+console.log ("Question: 2");
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+
+// Question 3
+console.log ("Question: 3");
+
+heading.style.fontSize = "2em";
+
+
+// Question 4
+console.log ("Question: 4");
+
+heading.classList.add("subheading");
+
+
+// Question 5
+console.log ("Question: 5");
+
+const paragraphs = document.querySelectorAll("p");
+
+for (i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = ("red");
+}
+
+
+// Question 6
+console.log ("Question: 6");
+
+const resultsContainer = document.querySelector("div.results");
+
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+
+// Question 7
+console.log ("Question: 7");
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function myFunction (list) {
+    for (let cat of cats) {
+        console.log (cats[i].name);
+    }
+}
+
+myFunction(list);
+
+
+// Question 8
+console.log ("Question: 8");
+
+
